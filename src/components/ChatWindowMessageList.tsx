bd87cfd17ab8ecbcdbaf5bc440eeec7c6e8703ab
Luis <simosa37@gmail.com>
2023-12-09 09:55:20 -0300

    format & style added to assitant text in panel

--- src/components/ChatWindowMessageList.tsx
+++ src/components/ChatWindowMessageList.tsx
@@ -1,18 +1,38 @@
 import { Message } from "@/types";
+import * as showdown from "showdown";
 import React from "react";
 
 const ChatWindowMessageList = ({ chat }: { chat: Message[] }) => {
+  const converter = new showdown.Converter();
+
   return (
     <div className={`h-full flex flex-col space-y-4 p-4`}>
       <div className="grow shrink basis-0 overflow-y-scroll scrollbar">
         {chat.map((e) => {
+          const isAssistant = e.author === "asistente";
+
           return (
             <div
               key={`chat_window_message_${e.id}`}
-              className="mb-2 pr-2"
+              className="mb-4 pr-2"
             >
-              <span className="font-semibold">{e.author}: </span>
-              <span>{e.content}</span>
+              <span
+                className={`font-semibold text-md capitalize ${
+                  isAssistant ? "text-green-500" : "text-blue-500"
+                }`}
+              >
+                {e.author}:{" "}
+              </span>
+              {isAssistant ? (
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: converter.makeHtml(e.content),
+                  }}
+                  className=""
+                ></span>
+              ) : (
+                <span className="">{e.content}</span>
+              )}
             </div>
           );
         })}
